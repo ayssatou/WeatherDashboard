37fa1494dbb23b05db7ed0e7eab0200e377f8dd6
ayssatou <alyssatou@outlook.com>
2021-09-26 19:52:22 -0400

    adding localStorage

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# WeatherDashboard
- 
+<h1> Weather Dashboard </h1>
+
+<p> The Application is composed of only one page. The user can search for cities using the input and button at the top left corner and the current weather information are displayed
+in the large box, the 5 day forecast is displayed in the smaller grey boxex. </p>
+
+<p>When the user search a city, it gets added to the history as a grey button under the search widget. If a city is searched again or clicked, it moved to the top of the history. When more than 10 cities are added, the older search gets removed from the history.</p>
+
+<p>The local storage is limited to a certain size in most browser so the application also automatically clean the events from the previous days.</p>
+
+<p> You can access the website at the following live address : <a href="https://ayssatou.github.io/WorkDayScheduler/">https://ayssatou.github.io/WorkDayScheduler/</a></p>
+
+<h1> Screenshots </h1>
+
+<p> Main Page </p>
+<img src="assets/images/workscheduler-main.png" title="scheduler-screenshot" alt="Screenshot of the main page of the scheduler"/>
+
+<p> The save buttons </p>
+<img src="assets/images/workscheduler-save.png" title="save-buttons-screenshot" alt="Screenshot of the save buttons"/>
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
+$(document).ready(function () {
+    var cityList = localStorage.cityList && localStorage.cityList !== undefined
+    ? JSON.parse(localStorage.cityList)
+    : [];
+
+    for(var i = 0; i < cityList.length; i++){
+        addCity(cityList[i],false);
+    }
+});
+
 function getIntDate(offset) {
   var dt = new Date();
   dt.setDate(dt.getDate() + offset);
@@ -19,35 +29,35 @@ function searchCity(city) {
     .then(function (findresponse) {
       return findresponse.json();
     })
-    .then(function (finddata){
-        fetch(
+    .then(function (finddata) {
+      fetch(
+        encodeURI(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${finddata.coord.lat}&lon=${finddata.coord.lon}&appid=c49757d1bfcf6a2d0dc2080d33a587ae&units=metric`
+        )
+      )
+        .then(function (onecallresponse) {
+          return onecallresponse.json();
+        })
+        .then(function (onecalldata) {
+          fetch(
             encodeURI(
-              `https://api.openweathermap.org/data/2.5/onecall?lat=${finddata.coord.lat}&lon=${finddata.coord.lon}&appid=c49757d1bfcf6a2d0dc2080d33a587ae&units=metric`
+              `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c49757d1bfcf6a2d0dc2080d33a587ae&units=metric`
             )
           )
-          .then(function (onecallresponse){
-            return onecallresponse.json();
-          })
-          .then(function (onecalldata){
-            fetch(
-                encodeURI(
-                  `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c49757d1bfcf6a2d0dc2080d33a587ae&units=metric`
-                )
-              )
-                .then(function (forecastresponse) {
-                  return forecastresponse.json();
-                })
-                .then(function (forecastdata) {
-                    addCity(city);
-                    applyData(city, onecalldata, forecastdata)
-                })
-                .catch(function (err) {
-                  console.log("Fetch Error : Forecast", err);
-                });
-          })
-          .catch(function (err) {
-            console.log("Fetch Error : OneCall", err);
-          });
+            .then(function (forecastresponse) {
+              return forecastresponse.json();
+            })
+            .then(function (forecastdata) {
+              addCity(city);
+              applyData(city, onecalldata, forecastdata);
+            })
+            .catch(function (err) {
+              console.log("Fetch Error : Forecast", err);
+            });
+        })
+        .catch(function (err) {
+          console.log("Fetch Error : OneCall", err);
+        });
     })
     .catch(function (err) {
       console.log("Fetch Error : Find", err);
@@ -55,28 +65,40 @@ function searchCity(city) {
 }
 
 function applyData(city, data, forecast) {
-    console.log(data);
-    console.log(forecast);
-    $("#data-city").text(city);
-    var date = new Date(parseInt(data.current.dt + "000"));
-    $("#data-date").text(date.getUTCMonth() + "/" + date.getUTCDate() + "/" + date.getUTCFullYear());
-    $("#cur-temp").text(data.current.temp);
-    $("#cur-wind").text(data.current.wind_speed);
-    $("#cur-hum").text(data.current.humidity);
-    $("#cur-uvi").text(data.current.uvi);
-    $("#cur-icon").attr("src",`https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`);
+  console.log(data);
+  console.log(forecast);
+  $("#data-city").text(city);
+  var date = new Date(parseInt(data.current.dt + "000"));
+  $("#data-date").text(
+    date.getUTCMonth() + "/" + date.getUTCDate() + "/" + date.getUTCFullYear()
+  );
+  $("#cur-temp").text(data.current.temp);
+  $("#cur-wind").text(data.current.wind_speed);
+  $("#cur-hum").text(data.current.humidity);
+  $("#cur-uvi").text(data.current.uvi);
+  $("#cur-icon").attr(
+    "src",
+    `https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`
+  );
 
-    for(var i = 0; i < 5; i++){
-        date = new Date(parseInt(forecast.list[i*8].dt + "000"));
-        $("#forecast-date-" + (i+1)).text(date.getUTCMonth() + "/" + date.getUTCDate() + "/" + date.getUTCFullYear());
-        $("#forecast-temp-" + (i+1)).text(forecast.list[i*8].main.temp);
-        $("#forecast-wind-" + (i+1)).text(forecast.list[i*8].wind.speed);
-        $("#forecast-hum-" + (i+1)).text(forecast.list[i*8].main.humidity);
-        $("#forecast-icon-" + (i+1)).attr("src",`https://openweathermap.org/img/wn/${forecast.list[i*8].weather[0].icon}@2x.png`);
-    }
+  for (var i = 0; i < 5; i++) {
+    date = new Date(parseInt(forecast.list[i * 8].dt + "000"));
+    $("#forecast-date-" + (i + 1)).text(
+      date.getUTCMonth() + "/" + date.getUTCDate() + "/" + date.getUTCFullYear()
+    );
+    $("#forecast-temp-" + (i + 1)).text(forecast.list[i * 8].main.temp);
+    $("#forecast-wind-" + (i + 1)).text(forecast.list[i * 8].wind.speed);
+    $("#forecast-hum-" + (i + 1)).text(forecast.list[i * 8].main.humidity);
+    $("#forecast-icon-" + (i + 1)).attr(
+      "src",
+      `https://openweathermap.org/img/wn/${
+        forecast.list[i * 8].weather[0].icon
+      }@2x.png`
+    );
+  }
 }
 
-function addCity(city) {
+function addCity(city, register = true) {
   $(".city-btn").each(function (index, element) {
     if ($(element).text().trim() === city || index > 9) {
       $(element).remove();
@@ -85,4 +107,12 @@ function addCity(city) {
   $("#cities").prepend(
     `<button class="row city-btn btn btn-secondary" onclick="searchCity('${city}')">${city}</button>`
   );
+
+  if (register) {
+    var cityList = [];
+    $(".city-btn").each(function (index, element) {
+      cityList.push($(element).text().trim());
+    });
+    localStorage.cityList = JSON.stringify(cityList);
+  }
 }
